bcacfe4f591d21a8a55f2398b9a83edc74fe9a18
leonardozecchin <51205734+leonardozecchin@users.noreply.github.com>
2020-12-01 12:32:46 +0100

    Aggiunti manualmente i file
    Ho aggiunto manualmente le cartelle e i file

---
+++ src/main/java/demo/AppController.java
@@ -0,0 +1,108 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private PersonRepository repository;
+
+    @RequestMapping("/")
+    public String index(){
+        return "list";
+    }
+
+    @RequestMapping("/list")
+    public String list(Model model){
+        List<Person> data = new LinkedList<>();
+        for (Person p: repository.findAll()){
+            data.add(p);
+        }
+        model.addAttribute("people", data);
+        return "list";
+    }
+
+    @RequestMapping("/input")
+    public String input(){
+        return "input";
+    }
+
+    @RequestMapping("/create")
+    public String create(
+            @RequestParam(name="firstname", required=true) String firstname,
+            @RequestParam(name="lastname", required=true) String lastname) {
+        repository.save(new Person(firstname,lastname));
+        return "redirect:/list";
+    }
+
+
+
+    @RequestMapping("/show")
+    public String read(
+            @RequestParam(name="id", required=true) Long id,
+            Model model) {
+        Optional<Person> result = repository.findById(id);
+        if(result.isPresent()){
+            model.addAttribute("person",result.get());
+            return "/show";
+        }
+        return "/notfound";
+    }
+
+
+
+    @RequestMapping("/edit")
+    public String edit(
+            @RequestParam(name="id", required=true) Long id,
+            Model model) {
+        Optional<Person> result = repository.findById(id);
+        if(result.isPresent()){
+            model.addAttribute("person",result.get());
+            return "edit";
+        }
+        else{ return  "notfound"; }
+
+    }
+
+    @RequestMapping("/update")
+    public String update(
+            @RequestParam(name="id", required=true) Long id,
+            @RequestParam(name="firstname", required=true) String firstname,
+            @RequestParam(name="lastname", required=true) String lastname,
+            Model model) {
+        Optional<Person> result = repository.findById(id);
+        if(result.isPresent()){
+            repository.delete(result.get());
+            repository.save(new Person(firstname,lastname));
+            return "redirect:/list";
+        }
+        else{
+            return "notfound";
+        }
+    }
+
+
+    @RequestMapping("/delete")
+    public String delete(
+            @RequestParam(name="id", required=true) Long id) {
+        Optional<Person> result = repository.findById(id);
+        if(result.isPresent()){
+            repository.delete(result.get());
+            return "redirect:/list";
+            //I didn't implement the 'add new person' for me it is not necessery in this case
+        }
+        else{
+            return "notfound";
+        }
+    }
+
+}
---
+++ src/main/java/demo/PersonRepository.java
@@ -0,0 +1,11 @@
+package demo;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+        List<Person> findByLastName(String lastName);
+
+        Person findById(long id);
+    }
