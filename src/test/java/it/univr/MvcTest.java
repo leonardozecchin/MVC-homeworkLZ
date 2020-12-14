b5e382c2ee252fc1674d92fe908bdf7b96988c72
leonardozecchin <leonardozecchin3@gmail.com>
2020-12-14 15:53:15 +0100

    Aggiunti altri test

--- src/test/java/it/univr/MvcTest.java
+++ src/test/java/it/univr/MvcTest.java
@@ -1,9 +1,13 @@
 package it.univr;
 
+import net.sourceforge.jwebunit.api.IElement;
 import net.sourceforge.jwebunit.junit.WebTester;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.platform.engine.TestExecutionResult;
+
+import java.util.List;
 
 public class MvcTest {
 
@@ -18,12 +22,15 @@ public class MvcTest {
     @Test
     public void testInitialView(){
         tester.beginAt("/");
+
         tester.clickLinkWithText("Add new person");
     }
 
     @Test
     public void addNewPersonTest(){
         tester.beginAt("/");
+
+        int before =  tester.getElementsByXPath("//td").size(); //ritorna tutti gli elementi che hanno un nome td
         tester.clickLinkWithText("Add new person");
 
         tester.assertTextPresent("Create a new record"); // serve per controllare se nella pagina in cui mi trovo c'è quel testo li
@@ -32,6 +39,10 @@ public class MvcTest {
         tester.submit();
 
         tester.assertTextPresent("People List");
+        int after = tester.getElementsByXPath("//td").size();
+
+        Assert.assertTrue(after > before);
+
     }
 
 }
